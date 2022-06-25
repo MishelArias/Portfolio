ea8c830162fe7db9835bed47722d58991b704596
Mishel Arias <mikeari78@gmail.com>
2022-06-25 01:22:11 -0500

    mejoras menores: sass agregado

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 
 router.get("/home", (req, res,) => {
-    res.render("html/index.html");
+    res.render("html/index", {
+        urlOne: "/about",
+        urlTwo: "/projects",
+        urlThree: "/contact"
+    });
 });
 
 module.exports = router;
