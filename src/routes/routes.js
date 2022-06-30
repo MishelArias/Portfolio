966111d43f7b090464d866017ecf662a47b4542c
Mishel Arias <mikeari78@gmail.com>
2022-06-30 01:47:00 -0500

    Agregada la seccion about, mejoras en uso de ejs, apariencias agregadas en sass

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,8 +2,22 @@ const router = require("express").Router();
 
 router.get("/home", (req, res,) => {
     res.render("html/index", {
+        titleOne: "Sobre mi",
         urlOne: "/about",
+        titleTwo: "Proyectos",
         urlTwo: "/projects",
+        titleThree: "Contactame",
+        urlThree: "/contact"
+    });
+});
+
+router.get("/about", (req, res) => {
+    res.render("html/about", {
+        titleOne: "Home",
+        urlOne: "/home",
+        titleTwo: "Proyectos",
+        urlTwo: "/projects",
+        titleThree: "Contactame",
         urlThree: "/contact"
     });
 });
